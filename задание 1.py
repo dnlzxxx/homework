f5d9ca9cfef3b9fd1b7ce66d3cd95dca6a8eefe3
dnlzxxx <danilbrazgin3@gmail.com>
2024-09-08 13:29:33 +0500

    Add files via upload
    выполнил данил бразгин 10а класс

---
+++ задание 1.py
@@ -0,0 +1,6 @@
+first = int(input("Введите целое число: "))
+second = int(input("Введите целое число: "))
+if first==second:
+    print(f"Числа {first} и {second} равны")
+else:
+    print(f"Числа  {first} и {second} не равны :(")
